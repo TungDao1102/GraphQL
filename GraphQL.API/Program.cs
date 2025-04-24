059ce707b0491a1df5b36407fd9ff7f895749d05
Tung Dao <daotung492@gmail.com>
2025-04-24 21:33:59 +0700

    remove un-use code/package

--- GraphQL.API/Program.cs
+++ GraphQL.API/Program.cs
@@ -4,26 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-// Add services to the container.
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGraphQL();
 
 app.Run();
+
+public class Query
+{
+    public string SayHello(string name = "world") => $"Hello {name}";
+}
